6498c52195ac1222091b12eb960a232f0d2b481d
its-Frank <franklineorina6@gmail.com>
2026-05-26 14:42:21 +0300

    changed the favicon and meta data title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,37 @@
-import type React from "react"
-import { Inter } from "next/font/google"
-import { ThemeProvider } from "@/components/theme-provider"
-import "./globals.css"
-import type { Metadata } from "next"
+import type React from "react";
+import { Inter } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
+import "./globals.css";
+import type { Metadata } from "next";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Frankline Orina | Full Stack Developer",
-  description: "Portfolio website of Frankline Orina, a Full Stack Developer specializing in Node.js and Express.js",
-    generator: 'v0.dev'
-}
+  title: "Francis Wany | Full Stack Developer",
+  description:
+    "Portfolio website of Francis Wany, a Full Stack Developer specializing in Node.js and Express.js",
+  generator: "",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange
+        >
           {children}
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
 
-
-
-import './globals.css'
+import "./globals.css";
